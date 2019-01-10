bced643c7a8c90f2c670d9550bc5ceb8f8691039
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2019-01-10 11:24:56 +0530

    Fixes #3

--- pagehash.js
+++ pagehash.js
@@ -5,6 +5,21 @@ let hash = (data, algo) => crypto.createHash(algo).update(data).digest("hex")
 
 module.exports = async url => {
   return new Promise((resolve, reject) => {
+    request({
+      url: url,
+      method: "HEAD"
+    }, (_error, res, _body) => {
+      if (typeof res === "undefined") {
+        return
+      }
+      if (res.headers['content-length'] != undefined && res.headers['content-length'] > 10000000) {
+        reject({
+          url: url,
+          hashes: null,
+          code: "MAX_LIMIT_EXCEEDED",
+        })
+      }
+    })
     request(url, (error, res, body) => {
       if (!error && res.statusCode == 200) {
         resolve({
